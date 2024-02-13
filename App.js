ce3213df8c4ec5aa1b0d14471a84e0680151207c
Vishnu Vardhan Vankayalapati <v_vankalaya@uncg.edu>
2024-02-13 14:55:40 -0500

    updated react component

--- App.js
+++ App.js
@@ -1,11 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-
-const jsxHeading = (<h2> Namaste </h2>);
-
+const element = <h1>Hello, world!</h1>;
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(jsxHeading);  
-
-
+root.render(element);
