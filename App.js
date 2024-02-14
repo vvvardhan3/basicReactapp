9580ac99dffa13532198839bec43e81e2977f3bb
Vishnu Vardhan Vankayalapati <v_vankalaya@uncg.edu>
2024-02-13 22:11:16 -0500

    update

--- App.js
+++ App.js
@@ -1,6 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const element = <h1 className="heading">Hello, world!</h1>;
+// Funtional Component
+const Title = () => {
+    return <h1 className="heading">Hello, Vishnu!</h1>;
+};
+
+// Component Composition 
+const JsxElement = () => 
+    (
+    <div id="container">
+        <Title />
+        <h1>Will meet you soon! </h1>
+    </div>
+    );
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(element);
+root.render(<JsxElement />);
