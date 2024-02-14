182203754189be3daa6e33d40baab447540295dc
Vishnu Vardhan Vankayalapati <v_vankalaya@uncg.edu>
2024-02-13 21:26:55 -0500

    React model runned

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const element = <h1>Hello, world!</h1>;
+const element = <h1 className="heading">Hello, world!</h1>;
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(element);
